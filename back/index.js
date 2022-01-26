1bbe8b6dbe7d769b9c18b606bb38433eb1d66989
fw1eleven1 <fw1eleven1@naver.com>
2022-01-26 17:13:29 +0900

    insert API 추가

--- back/index.js
+++ back/index.js
@@ -9,6 +9,8 @@ const app = express();
 app.set('port', 5000);
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('OK');
@@ -23,19 +25,25 @@ app.get('/account', (req, res) => {
 })
 
 app.get('/account/:date', (req, res) => {
-  console.log(req.params.date);
   const date = req.params.date;
   connection.query('SELECT * FROM account WHERE DATE_FORMAT(date, "%Y-%m-%d") = ?', [date], (err, rows, fields) => {
     if (err) throw err;
-    console.log(rows);
     res.json(rows)
+  })
+})
+
+app.post('/account/save', (req, res) => {
+  const data = req.body;
+  const type = data.type;
+  connection.query(`INSERT INTO account (date, ${type}, description) values (?, ?, ?)`, [data.date, data.amount, data.description], (err, results) => {
+    if (err) throw err;
+    res.status(200).send('OK');
   })
 })
 
 app.get('/users', (req, res) => {
   connection.query('SELECT * FROM users', (err, rows, fields) => {
     if (err) throw err;
-    console.log(rows);
     res.json(rows);
     // res.send(rows);
   });
